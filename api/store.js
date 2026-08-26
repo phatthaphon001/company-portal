2891a8d952e244e670bebdad7e460bba5a5133b6
phatthaphon001 <phatthaphon.chaikham03@gmail.com>
2026-08-27 00:04:04 +0700

    Update store.js

--- api/store.js
+++ api/store.js
@@ -1,6 +1,6 @@
 import { redisReady, redisGet, redisSet, requireUser, isBanned, clientIp, logActivity, autoBackup } from './_lib.js';
 
-const ALLOWED_KEYS = ['channels', 'tasks', 'history', 'lastActiveDate', 'futureTasks', 'templates', 'trash'];
+const ALLOWED_KEYS = ['channels', 'tasks', 'history', 'lastActiveDate', 'futureTasks', 'templates', 'trash', 'metrics'];
 
 export default async function handler(req, res) {
   if (!redisReady()) {
